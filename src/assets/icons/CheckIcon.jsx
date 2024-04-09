8a7831a7c286780bb37f5456a0092c9fe9364428
jorgeabrahan <jorge24abrahan@gmail.com>
2024-04-08 22:34:22 -0600

    feat: Add project icons assets

---
+++ src/assets/icons/CheckIcon.jsx
@@ -0,0 +1,21 @@
+export const CheckIcon = () => {
+  return (
+    <svg
+      width='24px'
+      height='24px'
+      strokeWidth='1.5'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      color='currentColor'
+    >
+      <path
+        d='M5 13L9 17L19 7'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      ></path>
+    </svg>
+  )
+}
---
+++ src/assets/icons/QuestionMarkIcon.jsx
@@ -0,0 +1,28 @@
+export const QuestionMarkIcon = () => {
+  return (
+    <svg
+      width='24px'
+      height='24px'
+      strokeWidth='1.5'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      color='currentColor'
+    >
+      <path
+        d='M7.90039 8.07954C7.90039 3.30678 15.4004 3.30682 15.4004 8.07955C15.4004 11.4886 11.9913 10.8067 11.9913 14.8976'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      ></path>
+      <path
+        d='M12 19.01L12.01 18.9989'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      ></path>
+    </svg>
+  )
+}
